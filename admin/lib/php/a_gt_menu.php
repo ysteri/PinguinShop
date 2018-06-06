a6a3d9e41d9e74f4790903738f8419fcac6aa54d
Thoma <Thoma@192.168.0.26>
2018-06-06 16:37:25 +0200

    a_gt_menu.php

---
+++ admin/lib/php/a_gt_menu.php
@@ -0,0 +1,26 @@
+<nav class="navbar navbar-expand-md bg-dark navbar-dark ">
+    <!-- Brand -->
+
+
+    <!-- Toggler/collapsibe Button -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <!-- Navbar links -->
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul class="navbar-nav">
+            <li><a   href="index.php?page=accueil_admin" class="txtGras"><span class="glyphicon glyphicon-home"></span>Accueil</a></li>       
+            <li><a   href="index.php?page=affiche_reservation" class="txtGras"><span class="glyphicon glyphicon-euro"></span>Commande</a></li>
+
+            <li><a   href="index.php?page=tab_dynamique" class="txtGras"><span class="glyphicon glyphicon-leaf"></span>Pingouin</a></li>
+            <li><a   href="index.php?page=contact_admin" class="txtGras"><span class="glyphicon glyphicon-question-sign"></span>Contact</a></li>
+            <li><a href="#" class="icoRss" title="Rss"><i class="fa fa-rss"></i></a></li>
+            <li><a href="#" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+            <li><a href="#" class="icoTwitter" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="#" class="icoGoogle" title="Google +"><i class="fa fa-google-plus"></i></a></li>
+            <li><a href="#" class="icoLinkedin" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+        </ul>
+    </div> 
+</nav>
+
