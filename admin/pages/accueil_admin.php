2c3f63d6bbe626e6c32ab80ec65cb3686dca2d95
Thoma <Thoma@192.168.0.26>
2018-06-06 19:12:01 +0200

    accueil_admin.php

---
+++ admin/pages/accueil_admin.php
@@ -0,0 +1,32 @@
+<h2 id="titre">ESPACE ADMINISTRATEUR</h2>
+
+<div class="row">
+    <div class="col-sm-8">
+        <div id="gt_carousel" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img id="carousel" class="d-block w-100" src="./images/carousel4.jpg" alt="dial">
+                </div>
+                <div class="carousel-item">
+                    <img id="carousel" class="d-block w-100" src="./images/carousel5.jpg" alt="anniversaire">
+                </div>
+                <div class="carousel-item" >
+                    <img id="carousel" class="d-block w-100" src="./images/carousel6.jpg" alt="etalage">
+                </div>
+
+            </div>
+            <a class="carousel-control-prev" href="#gt_carousel" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#gt_carousel" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>
+    <div class="col-sm-4">
+        Bienvenue
+    </div>
+</div>
+
