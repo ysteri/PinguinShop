ed45d943961dcc4c1510242566475d4f508f5550
Thoma <Thoma@192.168.0.26>
2018-06-06 15:51:12 +0200

    correction page accueil

--- pages/accueil.php
+++ pages/accueil.php
@@ -1,46 +1,45 @@
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 
 <h2 id="titre">Bienvenue</h2>
 
 <!--   instanciation ici-->
 <?php
-    $accueilBD = new AccueilBD($cnx);
-    $description = $accueilBD->getTexteAccueil();
+$accueilBD = new AccueilBD($cnx);
+$description = $accueilBD->getTexteAccueil();
 ?>
 <div class="row">
     <div class="col-md-7">
         <div class="carousel slide" id="gt_carousel" data-ride="carousel">  
             <div class="carousel-inner">
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img class="d-block w-100" id="carousel" src="./admin/images/carousel4.jpg" alt="dial">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="d-block w-100" id="carousel" src="./admin/images/carousel5.jpg" alt="anniversaire">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="d-block w-100" id="carousel" src="./admin/images/carousel6.jpg" alt="etalage">
-                    </div>
+                <div class="carousel-item active">
+                    <img class="d-block w-100" id="carousel" src="./admin/images/carousel4.jpg" alt="dial">
                 </div>
-                <a class="carousel-control-prev" href="#gt_carousel" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#gt_carousel" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
+                <div class="carousel-item">
+                    <img class="d-block w-100" id="carousel" src="./admin/images/carousel5.jpg" alt="anniversaire">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" id="carousel" src="./admin/images/carousel6.jpg" alt="etalage">
+                </div>
             </div>
-        </div>
-        <div class="col-sm-4">
-            <span class="txtGras "><p><br />Pendant votre absence, nous aurons &agrave; coeur le bien-&ecirc;tre des petits 
-                    compagnons que vous nous confierez !</span></p>
-            <p><br /><span class="txtItalic">Attention : </span><span class="petite_taille_texte">Pour preserver le confort de nos pensionnaires, nous limitons volontairement notre capacite d'accueil.<br />
-            Veuillez donc reserver au moins trois mois avant votre depart ! </span></p>
+            <a class="carousel-control-prev" href="#gt_carousel" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#gt_carousel" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
         </div>
     </div>
-      
+    <div class="col-sm-4">
+        <span class="txtGras "><p><br />Pendant votre absence, nous aurons &agrave; coeur le bien-&ecirc;tre des petits 
+                compagnons que vous nous confierez !</span></p>
+        <p><br /><span class="txtItalic">Attention : </span><span class="petite_taille_texte">Pour preserver le confort de nos pensionnaires, nous limitons volontairement notre capacite d'accueil.<br />
+                Veuillez donc reserver au moins trois mois avant votre depart ! </span></p>
+    </div>
+</div>
+
