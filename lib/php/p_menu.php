de855aad3ea2aa45baf26c6b71150b1ae868dda7
Thoma <Thoma@192.168.0.26>
2018-06-06 15:59:34 +0200

    p_menu.php

---
+++ lib/php/p_menu.php
@@ -0,0 +1,21 @@
+
+<nav class="navbar navbar-expand-md bg-dark navbar-dark ">
+    <!-- Brand -->
+
+
+    <!-- Toggler/collapsibe Button -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <!-- Navbar links -->
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul class="navbar-nav">
+            <li><a href="index.php?page=accueil.php" class="txtGras"><span class="glyphicon glyphicon-home"></span>  Accueil        </a></li>
+            <li><a href="index.php?page=commande.php" class="txtGras"><span class="glyphicon glyphicon-euro"></span>  Commander   </a></li>
+            <li><a href="index.php?page=catalogue.php" class="txtGras"><span class="glyphicon glyphicon-leaf"></span> Pingouins    </a> </li>
+            <li><a href="index.php?page=contact.php" class="txtGras"><span class="glyphicon glyphicon-question-sign"></span>  Contact      </a></li>
+        </ul>
+    </div> 
+</nav>
+
