c7b7ad1c40fc9b713a92577eac15d7bde034a568
Thoma <Thoma@192.168.0.26>
2018-06-06 18:02:09 +0200

    correction accueil.php

--- pages/accueil.php
+++ pages/accueil.php
@@ -1,9 +1,3 @@
-/* 
-* To change this license header, choose License Headers in Project Properties.
-* To change this template file, choose Tools | Templates
-* and open the template in the editor.
-*/
-
 <h2 id="titre">Bienvenue</h2>
 
 <div class="row">
